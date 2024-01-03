c812743ee940abd33b73d2ad5aba891495c85b7b
Farzanaltafbhat <97350316+Farzanaltafbhat@users.noreply.github.com>
2024-01-03 12:08:42 -0800

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,14 +3,7 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqm27vrlZjEdR0gPI8lto8CQ2-atZCIjs",
-  authDomain: "note-f4233.firebaseapp.com",
-  databaseURL: "https://note-f4233-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "note-f4233",
-  storageBucket: "note-f4233.appspot.com",
-  messagingSenderId: "461837570904",
-  appId: "1:461837570904:web:92ee14a91f6f4f7051cb2d",
-  measurementId: "G-4KVTH656P4"
+  
 };
 
 const app = initializeApp(firebaseConfig);
