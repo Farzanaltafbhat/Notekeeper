3feb574e3c90ddf7a6491426acf1585896860ae9
Farzanaltafbhat <97350316+Farzanaltafbhat@users.noreply.github.com>
2024-01-04 01:32:38 -0800

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,18 +3,18 @@ import { HeroProps } from '../../types';
 
 const Hero = ({ setShowModal }: HeroProps) => {
   return (
-    <div className="sticky top-0 bg-white shadow mb-4 rounded-xl p-2 flex justify-between">
-      <h1 className="text-base md:text-3xl h-full font-semibold p-3 my-auto">
+    <div className="sticky top-0 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 shadow mb-4 rounded-xl p-2 flex justify-between">
+      <h1 className="text-lg md:text-3xl text-white font-semibold p-3 my-auto">
         "When your heart speaks, take good notes."
       </h1>
       {/* Note add button */}
       <button
-        className="hover:bg-purple-100 text-purple-600 rounded-full font-bold uppercase text-sm px-4 py-2 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="hover:bg-purple-400 text-white rounded-full font-bold uppercase text-sm px-4 py-2 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 flex items-center"
         type="button"
         onClick={() => setShowModal(true)}
       >
         <img
-          style={{ width: 100 }}
+          className="w-6 h-6 mr-2"
           src={addImg}
           alt="+"
         />
