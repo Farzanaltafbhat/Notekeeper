6adbb5f449b3e6a7b905307960e8a39346470111
Farzanaltafbhat <97350316+Farzanaltafbhat@users.noreply.github.com>
2024-01-04 01:33:01 -0800

    Update LoadingScreen.tsx

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,26 +1,23 @@
 const LoadingScreen = () => {
   const size = 100;
+
   return (
     <>
-      <div className="border border-purple-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-        <div className="animate-pulse flex space-x-4">
+      <div className="border border-purple-500 bg-gradient-to-r from-purple-600 via-purple-700 to-purple-800 shadow-lg rounded-md p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4 items-center justify-center">
           <div
             style={{ width: `${size}px`, height: `${size}px` }}
-            className="animate-spin"
-          >
-            <div
-              className="h-full w-full border-2 border-t-purple-500 border-b-purple-700 rounded-[50%]"
-            ></div>
-          </div>
+            className="animate-spin rounded-full border-t-4 border-purple-300 border-b-4 border-purple-500"
+          ></div>
           <div className="flex-1 space-y-6 py-1">
-            <div className="h-2 bg-purple-700 rounded"></div>
+            <div className="h-3 bg-purple-500 rounded w-1/2"></div>
             <div className="space-y-3">
               <div className="grid grid-cols-3 gap-4">
-                <div className="h-2 bg-purple-700 rounded col-span-1"></div>
-                <div className="h-2 bg-purple-700 rounded col-span-2"></div>
+                <div className="h-3 bg-purple-500 rounded col-span-1"></div>
+                <div className="h-3 bg-purple-500 rounded col-span-2"></div>
               </div>
-              <div className="h-2 bg-purple-700 rounded"></div>
-              <div className="h-2 bg-purple-700 rounded"></div>
+              <div className="h-3 bg-purple-500 rounded w-3/4"></div>
+              <div className="h-3 bg-purple-500 rounded w-1/2"></div>
             </div>
           </div>
         </div>
